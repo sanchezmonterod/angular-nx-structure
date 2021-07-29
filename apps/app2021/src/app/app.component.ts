2716704ccdcfb42b8550944a3fc1e3f71fb0f106
David S <dsanchezm@vectoritcgroup.com>
2021-07-29 13:48:26 +0200

    initial config

---
+++ apps/app2021/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-nx-structure-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app2021';
+}
